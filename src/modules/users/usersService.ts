0be02ebbf30a6917525dceeef2965cee0aea98ce
sillascavalcanti <sillas.cavalcanti@liferay.com>
2024-08-07 13:40:52 -0300

    Feat:Add type to userService

--- src/modules/users/usersService.ts
+++ src/modules/users/usersService.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { UserModeles } from "./usersModules";
+import { UserInsertDTO } from "./dtos/userInsertDTO";
 
 const prisma = new PrismaClient();
 
-export const getUsers = async () => {
+export const getUsers = async (): Promise<UserModeles[]> => {
     return prisma.user.findMany();
 };
 
-export const addUser = async (body) => {
-    return prisma.user.create({ data: body });
+export const addUser = async (body:UserInsertDTO):Promise<UserModeles> => {
+    return prisma.user.create({
+         data: body 
+        });
 };
