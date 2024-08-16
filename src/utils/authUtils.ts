54dc1f84d6a0603681c95b089e02cfc4e8aee932
sillascavalcanti <sillas.cavalcanti@liferay.com>
2024-08-16 14:03:38 -0300

    SF

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -2,15 +2,16 @@ import { AuthException } from "@exceptions/authException";
 import { AuthUserDTO } from "@modules/auth/dtos/authDTO";
 import { UserModeles } from "@modules/users/usersModules";
 import { sign, verify } from "jsonwebtoken";
+import { json } from "stream/consumers";
 
 export const tokenpassword = "umasenha";
 
 export const generateToken = (user: UserModeles): string => {
     return sign(
         {
-            userid: user.id,
+            userId: user.id,
             email: user.email,
-            typeuser: user.typeUser,
+            typeUser: user.typeUser,
         },
         tokenpassword,
         {
@@ -20,13 +21,15 @@ export const generateToken = (user: UserModeles): string => {
     );
 };
 
-export const verifyToken = async (autorization: string): Promise<AuthUserDTO> => {
-    
-    if(!autorization){
+export const verifyToken = async (
+    authorization: string
+): Promise<AuthUserDTO> => {
+    if (!authorization) {
         throw new AuthException();
     }
-    
-    const [, token] = autorization.split(" ");
+
+    const [, token] = authorization.split(" ");
+
     try {
         const decodedToken = <AuthUserDTO>verify(token, tokenpassword);
 
