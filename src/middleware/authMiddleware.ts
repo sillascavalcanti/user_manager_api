423d77150954e0afe63bb2c48c659a48f0aaab23
sillascavalcanti <sillas.cavalcanti@liferay.com>
2024-08-16 14:03:19 -0300

    Feat: Create middleware to check admin roule

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,3 +1,5 @@
+import { Usertype } from "@enums/userEnum";
+import { AuthException } from "@exceptions/authException";
 import { ReturnError } from "@exceptions/dtos/exceptionDTO";
 import { verifyToken } from "@utils/authUtils";
 import { NextFunction, Request, Response } from "express";
@@ -16,4 +18,26 @@ export const authMiddleware = async (
         .catch((error) => {
             new ReturnError(res, error);
         });
+};
+
+export const authAdminMiddleware = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const token = req.headers.authorization;
+        console.log("token", token);
+        const user = await verifyToken(token);
+
+        if (user.typeUser !== Usertype.ADMIN) {
+            console.log(user.typeUser);
+            throw new AuthException();
+        }
+
+        next();
+    } catch (error) {
+        new ReturnError(res, error);
+        next(error);
+    }
 };
