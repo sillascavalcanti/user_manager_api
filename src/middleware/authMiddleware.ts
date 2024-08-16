b2afaa8f329109f270f24fc68e6ddd7cb15399df
sillascavalcanti <sillas.cavalcanti@liferay.com>
2024-08-16 14:04:47 -0300

    Feat: modify authMiddleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -4,19 +4,20 @@ import { ReturnError } from "@exceptions/dtos/exceptionDTO";
 import { verifyToken } from "@utils/authUtils";
 import { NextFunction, Request, Response } from "express";
 
-export const authMiddleware = async (
+export const authMiddleware = (
     req: Request,
     res: Response,
     next: NextFunction
-): Promise<void> => {
+): void => {
     const token = req.headers.authorization;
 
-    await verifyToken(token)
+    verifyToken(token)
         .then(() => {
             next();
         })
         .catch((error) => {
             new ReturnError(res, error);
+            next(error);
         });
 };
 
